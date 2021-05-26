bdd4e8b07dff39f54f55c4e57466655cd0192f61
AJC <AlexCast001@gmail.com>
2021-05-25 18:39:54 -0700

    Transfer the server to standalone (rather than relying on /video endpoint).

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@
 *  later.
 */
 
+const sleep = require('sleep');
 const fs = require('fs');
 const express = require('express');
 const app = express();
@@ -17,10 +18,11 @@ const http = require('http').createServer(app);
 const host = '0.0.0.0'; //'192.168.1.164'
 const port = 80;
 
+// Only select files in video format.
 const videoPath = "testimages/";
-global.lastModified = 0;
-global.attempts = 0;
-global.currentFile = ""; // TODO: Default with error / starting .mov?
+const VIDEO_REGEX = /\w+.mp4/;
+global.currentTimestamp = 0;
+global.currentFile = "wait.mp4"; // Set to waiting by default.
 
 app.use(express.static(__dirname + '/public'));
 
@@ -28,16 +30,40 @@ app.get('/', (req, res) => {
 });
 
 app.get('/video', function (req, res) {
-    // Only select files in video format.
-    const VIDEO_REGEX = /\w+.mp4/;
+    // Send the video.
+    res.statusCode = 200;
+    res.send(global.currentFile);
+    console.log(global.currentTimestamp, global.currentFile);
+});
+
+async function startServer() {   
+    // Thank you to pass by reference :')  
+    var arg = {}
+    arg.attempts = 0;
     
-    if (global.attempts > 10)
+    while(arg.attempts < 10) {
+        await updateCurrentFile(arg);
+    }
+}
+
+async function updateCurrentFile(arg) {
+    // Transmission has not begun yet.
+    if(global.currentTimestamp == 0) {
+        global.currentFile = "wait.mp4";
+        arg.attempts = 0; // Keep attempts at 0. 
+    }
+    
+    // Transmission has ended, or enough errors have
+    // passed through causing an end of transmission.
+    if (arg.attempts > 10)
     {
-        res.send('ended.mp4');
+        global.currentFile = 'ended.mp4';
         return;
     }
-
-    let dir = fs.readdirSync('public/' + videoPath, options = {"withFileTypes": true});
+    
+    const dir = fs.readdirSync('public/' + videoPath, options = {"withFileTypes": true});
+    
+    // Find the next file & timestamp.
     var nextTime = null;
     var nextFile = null;
     
@@ -46,45 +72,49 @@ app.get('/video', function (req, res) {
         if (!file.isFile() || file.name.search(VIDEO_REGEX) < 0) {
             continue;
         }
-
+        
+        // Find the files' creation date. 
         let birthtime = fs.statSync('public/' + videoPath + file.name).birthtimeMs;
         
-        if (birthtime > global.lastModified && (!nextTime || birthtime < nextTime)) {
+        // If file is later than the current latest file, and less than the next best file,
+        // set this to the next best file.
+        if (birthtime > global.currentTimestamp && (!nextTime || birthtime < nextTime)) {
             nextTime = birthtime;
             nextFile = file.name;
         }
-        else if (birthtime === global.lastModified) {
-            global.currentFile = file.name;
-        }
+    }
+    
+    // If video has been updated, reset attempts, otherwise increment.
+    if(!nextTime){
+        arg.attempts += 1;
+        
+        // Send 'Interupted' transmission if looping too much.
+        if (arg.attempts < 5) {
+            global.currentFile = 'interrupt.mp4';
+        }  
+    } else {
+        arg.attempts = 0;
+        global.currentTimestamp = nextTime;
+        global.currentFile = videoPath + nextFile;
     }
 
-    if (!nextTime) {
-        // Video has not been updated, increment and loop current file.
-        global.attempts += 1;
-        nextFile = global.currentFile;
-    }
-    else {
-        // Video has been updated, update timestamp and reset attempts. 
-        global.attempts = 0;
-        global.lastModified = nextTime;
-    }
-    res.statusCode = 200;
-    if (global.lastModified == 0) {
-        // Waiting for Transmission - No media in directory yet.
-        res.send('wait.mp4');
-    }
+    // Sleep for Video Length.
+    return new Promise(resolve => {
+        setTimeout(() => { return resolve(1); }, getDurationOfVideoMs(global.currentFile));
+    });
+}
 
-    // CHANGE THIS PART IF YOU WANT THE LOOPS TO BE LONGER.
-    else if (global.attempts < 5) {
-        // Send valid video.
-        res.send(videoPath + nextFile);
-    }        
-    else {
-        // Send error video / 'awaiting transmission.'
-        res.send('interrupt.mp4');
-        global.attempts += 1;
-    }  
-});
+function getDurationOfVideoMs(filepath) {
+    // Reading duration of an mp4 from NodeJS:
+    // Adapted from https://gist.github.com/Elements-/cf063254730cd754599e
+    var buff = new Buffer.alloc(100);
+    const fd = fs.openSync(__dirname + '/public/testimages/' + 'TEST_ONE.mp4');
+    fs.readSync(fd, buff);
+    var start = buff.indexOf(new Buffer.from('mvhd')) + 17;
+    var timeScale = buff.readUInt32BE(start, 4);
+    var duration = buff.readUInt32BE(start + 4, 4);
+    return Math.floor(duration/timeScale) * 1000;
+}
 
 /*
 *  FUNCTION: server
@@ -98,3 +128,4 @@ app.get('/video', function (req, res) {
 http.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
 });
+startServer();
