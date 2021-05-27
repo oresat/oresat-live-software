45ce10f9a71bc13de72bd90fa02aabfdd7b2dd78
Umair Khan <ukn@comcast.net>
2021-05-27 15:10:57 -0700

    Polish up camera.py

--- camera/camera.py
+++ camera/camera.py
@@ -1,35 +1,75 @@
+# Imports
+import sys
+import time
+import argparse
 import threading
 import subprocess
-import time
-import sys
 
-# global variable x
-x = 0
+# Global variables
+ARGS = None
+NUM = 0
 
-def cam_capture():
-	subprocess.call(["build/./imagecapture", "/dev/v4l/by-id/usb-Empia._UVC_Video_Device_12345678901234567890-video-index0", "640", "480", sys.argv[1], sys.argv[2]])
+# Capture images for a single video
+def capture_img():
+    subprocess.call([ARGS.binary, ARGS.device, str(ARGS.video_x), str(ARGS.video_y), str(ARGS.fps), str(ARGS.spv)])
 
+# Encode single video
 def encode_vid():
-    filename = "data/videos/output" + str(x) + ".mp4"
-    subprocess.call(["ffmpeg", "-framerate", sys.argv[3], "-i", "data/encode_img/image%03d.ppm", "-c:v", "libx264", "-b:v", sys.argv[4],"-preset", "ultrafast", "-loglevel","quiet", "-an", filename])
+    filename = f"data/videos/output{NUM:04d}.mp4"
+    subprocess.call(["ffmpeg", "-framerate", str(ARGS.fps), "-i", "data/encode_img/image%03d.ppm", "-c:v", "libx264", "-b:v", f"{ARGS.bit_rate}k",
+                     "-preset", "ultrafast", "-loglevel", "quiet", "-an", "-y", filename])
 
-def main():
-    # creating threads
-    t1 = threading.Thread(target=cam_capture)
-    t2 = threading.Thread(target=encode_vid)
+# Do a single multithreaded image capture + video encode
+def make_video():
 
-    # start threads
+    # Create threads
+    t1 = threading.Thread(target = capture_img)
+    t2 = threading.Thread(target = encode_vid)
+
+    # Start threads (with a delay)
     t1.start()
-    time.sleep(1)
+    time.sleep(0.2)
     t2.start()
 
-    # wait until threads finish their job
-
+    # Wait until threads finish their job
     t1.join()
     t2.join()
 
+# Main script
 if __name__ == "__main__":
-    for each in range(5):
-        main()
-        x += 1
+
+    # Set up argument parser
+    # TODO: add sanity checks on user input
+    parser = argparse.ArgumentParser(description = "Capture and encode low-fps videos.")
+    parser.add_argument("-bin", "--binary", default = "./build/imagecapture",
+                        help = "Path to imagecapture binary.")
+    parser.add_argument("-dev", "--device", default = "/dev/v4l/by-id/usb-Empia._UVC_Video_Device_12345678901234567890-video-index0",
+                        help = "Path to video device.")
+    parser.add_argument("-vx", "--video-x", type = int, default = 640,
+                        help = "Horizontal resolution of video.")
+    parser.add_argument("-vy", "--video-y", type = int, default = 480,
+                        help = "Vertical resolution of video.")
+    parser.add_argument("-fps", "--frames-per-second", dest = "fps", type = int, required = True,
+                        help = "Desired frames per second. (Note: this will probably only work at or under 10fps.)")
+    parser.add_argument("-spv", "--seconds-per-video", dest = "spv", type = int, required = True,
+                        help = "Seconds per created video. Must divide total duration.")
+    parser.add_argument("-br", "--bit-rate", type = int, required = True,
+                        help = "Bit rate of H.264 encoded videos.")
+    parser.add_argument("-t", "--total-duration", type = int, required = True,
+                        help = "Total capture duration, in seconds.")
+
+    # Parse arguments
+    ARGS = parser.parse_args()
+
+    # Basic check -- make sure seconds per video divides total duration
+    if ARGS.total_duration % ARGS.spv != 0:
+        sys.exit("Seconds per video does not divide total duration.")
+
+    # Figure out how many times to loop
+    num_loops = ARGS.total_duration // ARGS.spv
+
+    # Make videos
+    for _ in range(num_loops):
+        make_video()
+        NUM += 1
         subprocess.call(["ls", "data/encode_img"])
