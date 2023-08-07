2b04207372fa3bf0c26b4a9925e978bb171fdd56
Alexander Vassilevski <alexander.vassilevski@gmail.com>
2023-08-07 04:36:23 +0100

    new debian package build that sets up rx to write mp4s to the hgs public video dir

--- server/oresat-live-software-server-service.sh
+++ server/oresat-live-software-server-service.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+/usr/sbin/startmonitor && /usr/bin/rx --dev mon0 /usr/local/sbin/oresat-live-software-server/public/video/ -e mp4 -v &
 cd /usr/local/sbin/oresat-live-software-server/
 sudo npm i
 mkdir -p public/video
