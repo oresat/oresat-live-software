f94317fda96955bcf02e86ce2fc0b75a8e974d89
Alex Vassilevski <vassil4@pdx.edu>
2023-11-19 04:33:40 +0000

    making sure node and npm versions are fixed to a specific version and that node library files are included in the RPM

--- server/build-deb.sh
+++ server/build-deb.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+NODE_VERS='12.22.12~dfsg-1~deb11u4'
+NPM_VERS='7.5.2+ds-2'
+sudo apt-get -y install nodejs=$NODE_VERS npm=$NPM_VERS
+npm i
 PKG_NAME=oresat-live-software-server
-PKG_VERS=0.0.0.2
+PKG_VERS=0.0.0.3
 mkdir -p $PKG_NAME-$PKG_VERS/{DEBIAN,usr/local/sbin/oresat-live-software-server/,lib/systemd/system}
 cat <<EOF > $PKG_NAME-$PKG_VERS/DEBIAN/control
 Architecture: arm64
-Depends: nodejs (>= 10.24), npm (>= 5.8.0), oresat-dxwifi-rx (>=0.1.0-0)
+Depends: nodejs (=$NODE_VERS), npm (=$NPM_VERS), oresat-dxwifi-rx (>=0.1.0-0)
 Description: Oresat Live Software Server: serves video from directory
 Homepage: https://github.com/oresat/oresat-live-software
 Maintainer: PSAS <oresat@pdx.edu>
@@ -24,7 +28,7 @@ fi
 EOF
 chmod 755 $PKG_NAME-$PKG_VERS/DEBIAN/postinst
 cp -a startmonitor /usr/sbin/
-cp -r  rx.sh package.json index.js package-lock.json public/ $PKG_NAME-$PKG_VERS/usr/local/sbin/oresat-live-software-server/
+cp -r  rx.sh package.json index.js package-lock.json public/ node_modules/ $PKG_NAME-$PKG_VERS/usr/local/sbin/oresat-live-software-server/
 cp $PKG_NAME.service $PKG_NAME-$PKG_VERS/lib/systemd/system/
 cp oresat-live-software-rx.service $PKG_NAME-$PKG_VERS/lib/systemd/system/
 dpkg-deb --build $PKG_NAME-$PKG_VERS/
