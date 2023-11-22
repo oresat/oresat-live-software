3784b95945cf3d17d5dea3cb5d07f9aa2f7a3f56
Alexander Vassilevski <vassil4@pdx.edu>
2023-11-22 03:58:08 +0000

    oresat-live-software-server 0.0.0.4 - added fixes for when apt cache is not current and needs to be cleared and updated; also added some handling/debug messages for errors and exiting script on them;

--- server/build-deb.sh
+++ server/build-deb.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-NODE_VERS='12.22.12~dfsg-1~deb11u4'
-NPM_VERS='7.5.2+ds-2'
-sudo apt-get -y install nodejs=$NODE_VERS npm=$NPM_VERS
-npm i
+set -x
 PKG_NAME=oresat-live-software-server
 PKG_VERS=0.0.0.4
-mkdir -p $PKG_NAME-$PKG_VERS/{DEBIAN,usr/local/sbin/oresat-live-software-server/,lib/systemd/system,etc/cron.d/}
-cat <<EOF > $PKG_NAME-$PKG_VERS/DEBIAN/control
+NODE_VERS='12.22.12~dfsg-1~deb11u4'
+NPM_VERS='7.5.2+ds-2'
+function err_exit(){
+  echo "Error: There was an issue with $*, exiting." >&2
+  exit 1;
+}
+sudo apt clean || err_exit "apt clean"
+sudo apt update || err_exit "apt update"
+sudo apt-get -y install nodejs=$NODE_VERS npm=$NPM_VERS || err_exit "There was a problem installing node and npm"
+npm i || err_exit "There was an issue with npm i"
+mkdir -p $PKG_NAME-$PKG_VERS/{DEBIAN,usr/local/sbin/oresat-live-software-server/,lib/systemd/system,etc/cron.d/} || err_exit "making the dkpg-deb dirs"
+sh -c "cat > $PKG_NAME-$PKG_VERS/DEBIAN/control <<EOF
 Architecture: arm64
 Depends: nodejs (=$NODE_VERS), npm (=$NPM_VERS), oresat-dxwifi-rx (>=0.1.0-0)
 Description: Oresat Live Software Server: serves video from directory
@@ -16,8 +23,9 @@ Package: $PKG_NAME
 Priority: optional
 Section: net
 Version: $PKG_VERS
-EOF
-cat <<EOF > $PKG_NAME-$PKG_VERS/DEBIAN/postinst
+EOF" || err_exit "writing to DEBIAN control file"
+sh -c "cat > $PKG_NAME-$PKG_VERS/DEBIAN/postinst <<EOF
+cat <<EOF > $PKG_NAME-$PKG_VERS/DEBIAN/
 #!/bin/sh
 set -e
 if [ "\$1" = "configure" ]; then
@@ -25,11 +33,12 @@ if [ "\$1" = "configure" ]; then
     systemctl enable $PKG_NAME
     systemctl start $PKG_NAME
 fi
-EOF
-chmod 755 $PKG_NAME-$PKG_VERS/DEBIAN/postinst
-cp -a startmonitor /usr/sbin/
-cp -r  rx.sh package.json index.js package-lock.json public/ node_modules/ $PKG_NAME-$PKG_VERS/usr/local/sbin/oresat-live-software-server/
-cp $PKG_NAME.service $PKG_NAME-$PKG_VERS/lib/systemd/system/
-cp oresat-live-software-rx.service $PKG_NAME-$PKG_VERS/lib/systemd/system/
-cp cron-clean-videos $PKG_NAME-$PKG_VERS/etc/cron.d/clean-old-oresat-videos
-dpkg-deb --build $PKG_NAME-$PKG_VERS/
+EOF" || err_exit "writing to DEBIAN postinst file"
+chmod 755 $PKG_NAME-$PKG_VERS/DEBIAN/postinst || err_exit "chmodding the postinst file"
+cp -a startmonitor /usr/sbin/ || err_exit "copying files to destination dir"
+cp -r  rx.sh package.json index.js package-lock.json public/ node_modules/ $PKG_NAME-$PKG_VERS/usr/local/sbin/oresat-live-software-server/ || err_exit "copying files to destination dir"
+cp $PKG_NAME.service $PKG_NAME-$PKG_VERS/lib/systemd/system/ || err_exit "copying files to destination dir"
+cp oresat-live-software-rx.service $PKG_NAME-$PKG_VERS/lib/systemd/system/ || err_exit "copying files to destination dir"
+cp cron-clean-videos $PKG_NAME-$PKG_VERS/etc/cron.d/clean-old-oresat-videos || err_exit "copying files to destination dir"
+dpkg-deb --build $PKG_NAME-$PKG_VERS/  || err_exit "dpkg-deb"
+set +x
